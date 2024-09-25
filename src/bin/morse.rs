bbc75045afcb53243b1043d4d10f2541ccd412b6
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-09-25 18:27:11 -0400

    feat(morse): add spacing

--- src/bin/morse.rs
+++ src/bin/morse.rs
@@ -1,34 +1,36 @@
+use itertools::Itertools;
 use std::collections::HashMap;
-use std::io::{self, Write};
+use std::io::Write;
+use std::iter::repeat;
 
 fn main() {
     let morse_rep = HashMap::from([
-        ('a', ".-"),
-        ('b', "-..."),
-        ('c', "-.-."),
-        ('d', "-.."),
-        ('e', "."),
-        ('f', "..-."),
-        ('g', "--."),
-        ('h', "...."),
-        ('i', ".."),
-        ('j', ".---"),
-        ('k', "-.-"),
-        ('l', ".-.."),
-        ('m', "--"),
-        ('n', "-."),
-        ('o', "---"),
-        ('p', ".--."),
-        ('q', "--.-"),
-        ('r', ".-."),
-        ('s', "..."),
-        ('t', "-"),
-        ('u', "..-"),
-        ('v', "...-"),
-        ('w', ".--"),
-        ('x', "-..-"),
-        ('y', "-.--"),
-        ('z', "--.."),
+        ('A', ".-"),
+        ('B', "-..."),
+        ('C', "-.-."),
+        ('D', "-.."),
+        ('E', "."),
+        ('F', "..-."),
+        ('G', "--."),
+        ('H', "...."),
+        ('I', ".."),
+        ('J', ".---"),
+        ('K', "-.-"),
+        ('L', ".-.."),
+        ('M', "--"),
+        ('N', "-."),
+        ('O', "---"),
+        ('P', ".--."),
+        ('Q', "--.-"),
+        ('R', ".-."),
+        ('S', "..."),
+        ('T', "-"),
+        ('U', "..-"),
+        ('V', "...-"),
+        ('W', ".--"),
+        ('X', "-..-"),
+        ('Y', "-.--"),
+        ('Z', "--.."),
         ('1', ".----"),
         ('2', "..---"),
         ('3', "...--"),
@@ -43,19 +45,29 @@ fn main() {
         ('?', "..--.."),
         (':', "---..."),
         ('&', ".-..."),
+        (' ', "/"),
     ]);
 
     let mut message = String::new();
-
     let stdin = std::io::stdin();
     let mut stdout = std::io::stdout();
+
     print!("Enter text to be encoded: ");
     stdout.flush().expect("Failed to flush output buffer");
+
     stdin
         .read_line(&mut message)
         .expect("Failed to read message");
-    let message = message.trim().to_lowercase();
+    let message = message.trim().to_uppercase();
     println!("Plain message: {}", message);
-    let encoded: String = message.chars().map(|c| morse_rep[&c]).collect();
-    println!("Encoded message: {}", encoded)
+
+    let spaces = repeat(" ");
+    let encoded: String = message
+        .chars()
+        .map(|c| morse_rep[&c])
+        .interleave_shortest(spaces)
+        .collect();
+    let encoded = encoded.trim();
+
+    println!("Encoded message: {}", encoded);
 }
